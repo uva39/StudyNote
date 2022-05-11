cefd2cbc09932b68757d288af928e3f6077cf3d2
36david07 <36david07@gmail.com>
2022-05-11 23:14:05 +0900

    v1.1

--- Sort_ALL_IN_ONE.py
+++ Sort_ALL_IN_ONE.py
@@ -3,8 +3,12 @@ RawData = [*eval("int(sys.stdin.readline().rstrip())," * int(input()))]
 # N개의 data를 한 줄에 받아오는 코드
 
 # TODO: 퀵정렬, 힙정렬, 기수정렬, 칵테일 정렬, 병합정렬, 셸 정렬 구현  and 파이썬 내장 정렬함수 사용법
+# 이후의 시간복잡도 표기는 전부 Best - Average - Worst (최선 - 평균 - 최악) 순으로 표기한다.
 
-# 선택정렬 - best: O(N^2) ~ worst: O(N^2)
+
+
+
+# 선택정렬: O(N^2) - O(N^2) - O(N^2)
 
 # 1. 바꿀 데이터의 인덱스를 i라 하고, 0(맨 앞)으로 초기화한다.
 # 2. O(n)의 시간복잡도로 배열에서 가장 작은 데이터를 찾은 뒤, arr[i]와 맞바꾼다.
@@ -19,7 +23,7 @@ def selection(arr):
                 min_idx = j
         arr[min_idx], arr[i] = arr[i], arr[min_idx]
 
-# 삽입 정렬 - best: O(N) ~ worst: O(N^2)
+# 삽입 정렬: O(n) - O(n^2) - O(n^2)
 
 # 정렬 과정에서 데이터가 들어가기에 적절한 위치를 찾은 뒤, 그 위치에 데이터를 삽입한다.
 # 필요할 때만 위치를 바꾸기 때문에, "거의 정렬된 데이터"에서 매우 효율적이다.
@@ -36,7 +40,7 @@ def insertion(arr):
             else:
                 break
 
-# 버블 정렬 - best: O(N^2) ~ worst: O(N^2)
+# 버블 정렬: O(n) - O(n^2) - O(n^2)
 
 # 서로 인접한 두 원소의 대소 관계를 비교해가며 정렬한다.
 
