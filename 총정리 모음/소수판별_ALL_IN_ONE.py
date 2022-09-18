d293ba571acf4d845a033a596f2284a57164c2ee
36david07 <36david07@gmail.com>
2022-09-18 19:29:00 +0900

    new

--- 총정리 모음/소수판별_ALL_IN_ONE.py
+++ 총정리 모음/소수판별_ALL_IN_ONE.py
@@ -3,105 +3,77 @@ import sys
 
 
 
-
-
-
-
-print('asdf')
-
-# NORMAL RANGE OF PRIME
-
-
-
-
-def isPrime_sqrt(n): # import math
-    t = int(math.sqrt(n)) + 1
+def isPrime_sqrt(n:int) -> bool:
+    """
+    소수인지 확인할 수 n을 입력받으면, sqrt(n)+1까지 쭉 나눠보고 답을 반환하는 함수입니다.
+    시간복잡도는 O(sqrt(N))이고, 이정도만 해도 충분히 쓸만합니다.
+    """
+    t = int(n**0.5) + 1
     for i in range(2, t):
         if n % i == 0:
             return False
-
     return True
 
-
-
-
-# SIMPLE
-
-
-
-
-def isPrime_simple(n): 
+def isPrime_simple(n:int) -> bool: 
+    """
+    isPrime_sqrt 함수와 동일한 알고리즘의 함수이지만, sqrt(n)까지 나누지 않고 쭉 나누는 함수입니다.
+    성능 상 위 함수의 하위호환입니다.
+    """
     for i in range(2, n):
         if n % i == 0:
             return False
-
     return True
 
 
 
 
-
-# MANY PRIMES
-
-
-
-
-def isPrime_Eras(n): # import math
-    check = [True] * n+1
-    for i in range(2, int(math.sqrt(n+2))+1):
+def isPrime_Eras(n:int) -> int:
+    """
+    에라스토테네스의 체를 이용하여 n 이하의 모든 소수를 구하는 방법입니다.
+    소수 1개만 단일로 구할 때도 isPrime_sqrt 함수와 시간복잡도 차이가 적고,
+    n 이하의 모든 소수를 구하는 경우, 이 방법이 가장 빠릅니다.
+    """
+    check = [True]*(n+1)
+    for i in range(2, int((n+2)**0.5)+1):
         if check[i]:
             for j in range(i*2, n+1, i):
                 check[j] = False
-    return check
+    return check[n]
 
 
 
 
-
-
-
-# ULTRA MANY , LARGE PRIMES
-
-
-
-
-
-
-
-
-
-def Miller_Rabin(n,a): # import math, import sys
+def Miller_Rabin(n:int,_a:int) -> bool:
+    """
+    밀러-라빈 소수판정법을 이용한 방법입니다. 페르마의 정리의 역이 꽤 높은 확률로 성립한다는 성질을 이용합니다.
+    소수가 담긴 리스트 A를 받아서, 그 원소에 있는 소수들에 대해서 밀러-라빈 판정법을 시행합니다.
+    """
     d = (n-1) // 2
     while d % 2 == 0:
-        if pow(a,d,n) == n-1:
+        if pow(_a,d,n) == n-1:
             return True
         d //= 2
-    t = pow(a,d,n)
-    return True if t == n-1 or t == 1 else False
+    t = pow(_a,d,n)
+    return True if t in (n-1, 1) else False
 
-def isPrime_MR_Eras(n):
+def isPrime_Miller(n:int, __E__ = [True] * 101) -> bool:
+    for i in range(2, int(math.sqrt(102))+1):
+        if __E__[i]:
+            for j in range(i*2, 101, i):
+                __E__[j] = False
     if n <= 1:
         return False
     if n <= 100:
-        return True if E[n] else False
-    # over is not Miller_Rabin Test. It's Erastotenes.
-    for a in A:
+        return True if __E__[n] else False
+    for a in [2, 7, 61]:
         if not Miller_Rabin(n,a):
             return False
     return True
         
 
-# non-function initial setting
-E = [True] * 101
-for i in range(2, int(math.sqrt(102))+1):
-    if E[i]:
-        for j in range(i*2, 101, i):
-            E[j] = False
-# 2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97
-# in usual, A = [2,7,61] is best.
-A = [2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97]
 
 # Test input for Super Big Integer. Is following number prime?
 # 24815323469403931728221172233738523533528335161133543380459461440894543366372904768334987264000000000000000000479
-num = int(sys.stdin.readline())
-print(isPrime_MR_Eras(num))
+if __name__ == '__main__':
+    num = int(sys.stdin.readline())
+    print(isPrime_Miller(num))
--- 총정리 모음/피보나치_ALL_IN_ONE.py
+++ 총정리 모음/피보나치_ALL_IN_ONE.py
@@ -1,63 +1,86 @@
-# 재귀함수 이용 - 시간복잡도 O(2^N)
 
 
 
 
-def fibo_a(n): # ~30까지 빠름
-    if n in (1,2):
+def fibo_a(n:int) -> int:
+    """
+    재귀함수를 이용한, 시간복잡도가 O(2^N)인 함수입니다.
+    구현이 가장 간단하지만, 그만큼 시간복잡도가 많이 불리하여, 잘 사용하지 않습니다.
+    """
+    if n == 0:
+        return 0
+    elif n == 1:
         return 1
     return fibo_a(n-1) + fibo_a(n-2)
 
 
 
 
-# 동적계획법 이용 - 시간복잡도 O(N)
-
-
-
-
-def fibo_b(n): # ~십만까지 빠름
-    cache = [0, 1]
+def fibo_b(n:int, __cache__ = [0, 1]) -> int:
+    """
+    동적계획법을 이용한, 시간복잡도가 O(N)인 함수입니다.
+    구현이 간단한데도 시간복잡도가 상당히 유리하기에 가장 많이 사용합니다.
+    
+    이 구현에선 추가로 함수의 클로저에 __cache__를 저장하여,
+    여러 번 사용 시에 값을 재활용할 수 있게끔 했습니다.
+    """
+    if n < len(__cache__):
+        return __cache__[n]
     for i in range(2, n+1):
-        cache.append(cache[i-1] + cache[i-2])
-    return cache[n]
+        __cache__.append(__cache__[i-1] + __cache__[i-2])
+    return __cache__[n]
 
 
-
-
-# 행렬곱셈 이용 - 시간복잡도 O(log(N))
 
 
 import sys
-sys.setrecursionlimit(2500)
+sys.setrecursionlimit(2500) # 분할정복 시에 재귀 깊이를 늘려주면 좋음
 
-def matrix_mult(A, B):
-    temp = [[0] * 2 for _ in range(2)]
-    for i in range(2):
-        for j in range(2):
+
+def matrix_mult(A:list, B:list) -> list:
+    """
+    2x2 shape의 행렬끼리 행렬곱셈을 하는 함수입니다.
+    피보나치 수를 구하는 과정에선 2x2 shape의 정사각행렬끼리만 곱셈을 수행하므로,
+    다른 shape의 행렬은 고려하지 않아도 됩니다.
+    """
+    temp = [[0]*2 for _ in range(2)] # 곱을 저장할 행렬. shape:(2, 2)
+    for i in range(2): # i: 행
+        for j in range(2): # j: 열
             for k in range(2):
                 temp[i][j] += (A[i][k] * B[k][j])
                 temp[i][j] %= 1000000007 # 더 큰 수를 얻기 위해, p로 나눈 나머지를 구하고싶다면 여기를 변경
     return temp
 
-def matrix_pow(n, M):
-    if n == 1:
+
+def matrix_pow(n:int, M:list) -> list:
+    """
+    행렬 M의 n제곱을 구하는 함수입니다.
+    matrix_mult 함수와 분할정복을 활용하여 재귀적으로 계산을 수행합니다.
+    """
+    if n == 1: # M^1 = M
         return M
     if n % 2 == 0:
-        temp = matrix_pow(n//2, M)
+        temp = matrix_pow(n//2, M) # m = n//2라 하면, M^n = M^(2m) = M^m * M^m.
         return matrix_mult(temp, temp)
     else:
-        temp = matrix_pow(n-1, M)
+        temp = matrix_pow(n-1, M) # m = n-1이라 하면, M^n = M^(m+1) = M^m * M^1
         return matrix_mult(temp, M)
 
-def fibo_c(n): # ~백만까지 빠름
-    a = ((1, 1), (1, 0))
-    if n == 0:
-        return 0
-    elif n == 1:
-        return 1
-    return matrix_pow(n- 1, a)[0][0]
 
-if __name__=="__main__":
+def fibo_c(n:int) -> int:
+    """
+    특수한 행렬에 대한 행렬곱셈(거듭제곱)을 이용하여,
+    시간복잡도 O(log(N))으로 피보나치 수를 구하는 함수입니다.
+    특수한 행렬 a의 거듭제곱으로 피보나치 수를 계산하기에, 앞에서 정의한 matrix_pow 함수를 활용합니다.
+    """
+    a = ((1, 1), (1, 0)) # 피보나치 수를 구할 때에 쓰이는 특수한 행렬
+    if n == 0: # Fibo(0) = 0
+        return 0
+    elif n == 1: # Fibo(1) = 1
+        return 1
+    return matrix_pow(n-1, a)[0][0] # A = a^(n-1)이라 하면, A_00이 정답이다.
+
+
+if __name__=="__main__": # Test
     n = int(input())
     print(fibo_c(n))
